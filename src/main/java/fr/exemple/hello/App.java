6b2431186198a6356eb7d0551cfffd54517dc4cc
DQJF9658 <DQJF9658@EB-OR6171212.ad.francetelecom.fr>
2019-11-04 16:56:32 +0100

    fix var name

--- src/main/java/fr/exemple/hello/App.java
+++ src/main/java/fr/exemple/hello/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Salut le monde" );
-        App2 app = new App2();
+        App2 app2 = new App2();
     }
 }
