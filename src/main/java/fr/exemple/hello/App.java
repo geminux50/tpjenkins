b31a8753b0032716e5799a5e2553763f848153af
DQJF9658 <DQJF9658@EB-OR6171212.ad.francetelecom.fr>
2019-11-04 17:19:14 +0100

    Add test and Op class

--- src/main/java/fr/exemple/hello/App.java
+++ src/main/java/fr/exemple/hello/App.java
@@ -9,6 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Salut le monde" );
-        App2 app2 = new App2();
+        
+        Operation op = new Operation();
+        int res = op.add(5, 3);
+        System.out.println( res );
+
+        
+        new App2();
     }
 }
---
+++ src/main/java/fr/exemple/hello/Operation.java
@@ -0,0 +1,24 @@
+package fr.exemple.hello;
+
+public class Operation {
+
+	public Operation() {
+
+	}
+
+	public int add(int one, int two) {
+		return one + two;
+	}
+	
+	public int sub(int one, int two) {
+		return one - two;
+	}
+	
+	public int mul(int one, int two) {
+		return one * two;
+	}
+	
+	public int div(int one, int two) {
+		return one / two;
+	}
+}
